f8d70ae001702b3d9d5b7514b151fff7768732f3
kapil9711 <kapil.rajput9711@gmail.com>
2025-07-07 20:03:15 +0530

    intial setup with tailwind and hero ui

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@heroui/react'
+
+function App(): React.JSX.Element {
+  return (
+    <div>
+      <Button color="primary">Button</Button>
+      <h1 className="text-4xl text-orange-600">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
